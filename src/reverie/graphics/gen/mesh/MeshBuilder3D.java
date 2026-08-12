0abc244274b73152c6119b576c4dd863304151c1
GglLfr <glenn.folk08@gmail.com>
2026-08-12 22:52:53 +0800

    feat: Enable multithreading for mesh building

--- src/reverie/graphics/gen/mesh/MeshBuilder3D.java
+++ src/reverie/graphics/gen/mesh/MeshBuilder3D.java
@@ -54,91 +54,96 @@ public final class MeshBuilder3D{
                 }
             }
         }
-        var indices = vertexCount < 65536 ? new short[3] : null;
 
-        var col = new Color();
-        var nor = new Vec3();
-        for(var tile : grid.tiles){
-            // TODO: `skip(Vec3)` is unsupported here; instead, skipping is only done when the two curves intersect.
-            //       Should it be supported? I have no such usecases...
-            if(skips.items[tile.id]) continue;
+        int vertexCountFinal = vertexCount;
+        Tasks.scope(scope -> Iterables.split(grid.tiles, 1024, (start, end) -> scope.get(() -> {
+            var indices = vertexCountFinal < 65536 ? new short[3] : null;
 
-            var c = tile.corners;
-            for(boolean isOutward : Mathf.booleans){
-                int cOffset = isOutward ? 0 : grid.corners.length;
+            var col = new Color();
+            var nor = new Vec3();
+            for(int tileIndex = start; tileIndex < end; tileIndex++){
+                var tile = grid.tiles[tileIndex];
+                // TODO: `skip(Vec3)` is unsupported here; instead, skipping is only done when the two curves intersect.
+                //       Should it be supported? I have no such usecases...
+                if(skips.items[tile.id]) continue;
 
-                Corner c1 = c[0], c2 = c[isOutward ? 2 : 4], c3 = c[isOutward ? 4 : 2];
-                float h1 = heights.items[cOffset + c1.id], h2 = heights.items[cOffset + c2.id], h3 = heights.items[cOffset + c3.id];
-                Vec3 v1 = c1.v, v2 = c2.v, v3 = c3.v;
+                var c = tile.corners;
+                for(boolean isOutward : Mathf.booleans){
+                    int cOffset = isOutward ? 0 : grid.corners.length;
 
-                normal(
-                    v1.x * h1, v1.y * h1, v1.z * h1,
-                    v2.x * h2, v2.y * h2, v2.z * h2,
-                    v3.x * h3, v3.y * h3, v3.z * h3,
-                    nor
-                );
+                    Corner c1 = c[0], c2 = c[isOutward ? 2 : 4], c3 = c[isOutward ? 4 : 2];
+                    float h1 = heights.items[cOffset + c1.id], h2 = heights.items[cOffset + c2.id], h3 = heights.items[cOffset + c3.id];
+                    Vec3 v1 = c1.v, v2 = c2.v, v3 = c3.v;
 
-                (isOutward ? outward : inward).getColor(tile.v, col.set(1f, 1f, 1f, 1f));
+                    normal(
+                        v1.x * h1, v1.y * h1, v1.z * h1,
+                        v2.x * h2, v2.y * h2, v2.z * h2,
+                        v3.x * h3, v3.y * h3, v3.z * h3,
+                        nor
+                    );
 
-                float color = col.toFloatBits();
-                float emissive;
-                if(isOutward ? emitOutward : emitInward){
-                    (isOutward ? outward : inward).getEmissiveColor(tile.v, col.set(0f, 0f, 0f, 0f));
-                    emissive = col.toFloatBits();
-                }else{
-                    emissive = 0f;
-                }
+                    (isOutward ? outward : inward).getColor(tile.v, col.set(1f, 1f, 1f, 1f));
 
-                if(indices != null){
-                    int position = builder.vertex(c.length, (verts, off) -> {
-                        for(var corner : c){
-                            float height = heights.items[cOffset + corner.id];
-
-                            builder.pos3d(verts, off, corner.v.x * height, corner.v.y * height, corner.v.z * height);
-                            builder.normal(verts, off, nor.x, nor.y, nor.z);
-                            builder.diffuse(verts, off, color);
-                            if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
-
-                            off += builder.stride;
-                        }
-                    });
-
-                    for(int i = 0; i < c.length - 2; i++){
-                        indices[0] = (short)(position);
-                        indices[1] = (short)(position + i + (isOutward ? 1 : 2));
-                        indices[2] = (short)(position + i + (isOutward ? 2 : 1));
-                        builder.indices(indices);
+                    float color = col.toFloatBits();
+                    float emissive;
+                    if(isOutward ? emitOutward : emitInward){
+                        (isOutward ? outward : inward).getEmissiveColor(tile.v, col.set(0f, 0f, 0f, 0f));
+                        emissive = col.toFloatBits();
+                    }else{
+                        emissive = 0f;
                     }
-                }else{
-                    for(int i = 0; i < c.length - 2; i++){
-                        float
-                            ca = heights.items[cOffset + c[0].id],
-                            cb = heights.items[cOffset + c[i + (isOutward ? 1 : 2)].id],
-                            cc = heights.items[cOffset + c[i + (isOutward ? 2 : 1)].id];
 
-                        int index = i;
-                        builder.vertex(3, (verts, off) -> {
-                            builder.pos3d(verts, off, c[0].v.x * ca, c[0].v.y * ca, c[0].v.z * ca);
-                            builder.normal(verts, off, nor.x, nor.y, nor.z);
-                            builder.diffuse(verts, off, color);
-                            if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
+                    if(indices != null){
+                        int position = builder.vertex(c.length, (verts, off) -> {
+                            for(var corner : c){
+                                float height = heights.items[cOffset + corner.id];
 
-                            off += builder.stride;
-                            builder.pos3d(verts, off, c[index + (isOutward ? 1 : 2)].v.x * cb, c[index + (isOutward ? 1 : 2)].v.y * cb, c[index + (isOutward ? 1 : 2)].v.z * cb);
-                            builder.normal(verts, off, nor.x, nor.y, nor.z);
-                            builder.diffuse(verts, off, color);
-                            if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
+                                builder.pos3d(verts, off, corner.v.x * height, corner.v.y * height, corner.v.z * height);
+                                builder.normal(verts, off, nor.x, nor.y, nor.z);
+                                builder.diffuse(verts, off, color);
+                                if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
 
-                            off += builder.stride;
-                            builder.pos3d(verts, off, c[index + (isOutward ? 2 : 1)].v.x * cc, c[index + (isOutward ? 2 : 1)].v.y * cc, c[index + (isOutward ? 2 : 1)].v.z * cc);
-                            builder.normal(verts, off, nor.x, nor.y, nor.z);
-                            builder.diffuse(verts, off, color);
-                            if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
+                                off += builder.stride;
+                            }
                         });
+
+                        for(int i = 0; i < c.length - 2; i++){
+                            indices[0] = (short)(position);
+                            indices[1] = (short)(position + i + (isOutward ? 1 : 2));
+                            indices[2] = (short)(position + i + (isOutward ? 2 : 1));
+                            builder.indices(indices);
+                        }
+                    }else{
+                        for(int i = 0; i < c.length - 2; i++){
+                            float
+                                ca = heights.items[cOffset + c[0].id],
+                                cb = heights.items[cOffset + c[i + (isOutward ? 1 : 2)].id],
+                                cc = heights.items[cOffset + c[i + (isOutward ? 2 : 1)].id];
+
+                            int index = i;
+                            builder.vertex(3, (verts, off) -> {
+                                builder.pos3d(verts, off, c[0].v.x * ca, c[0].v.y * ca, c[0].v.z * ca);
+                                builder.normal(verts, off, nor.x, nor.y, nor.z);
+                                builder.diffuse(verts, off, color);
+                                if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
+
+                                off += builder.stride;
+                                builder.pos3d(verts, off, c[index + (isOutward ? 1 : 2)].v.x * cb, c[index + (isOutward ? 1 : 2)].v.y * cb, c[index + (isOutward ? 1 : 2)].v.z * cb);
+                                builder.normal(verts, off, nor.x, nor.y, nor.z);
+                                builder.diffuse(verts, off, color);
+                                if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
+
+                                off += builder.stride;
+                                builder.pos3d(verts, off, c[index + (isOutward ? 2 : 1)].v.x * cc, c[index + (isOutward ? 2 : 1)].v.y * cc, c[index + (isOutward ? 2 : 1)].v.z * cc);
+                                builder.normal(verts, off, nor.x, nor.y, nor.z);
+                                builder.diffuse(verts, off, color);
+                                if(emitOutward || emitInward) builder.emissive(verts, off, emissive);
+                            });
+                        }
                     }
                 }
             }
-        }
+        })));
 
         floatArrayPool.free(heights);
         boolArrayPool.free(skips);
